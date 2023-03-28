9cf33c71af1e05176e48b2698585554119c210b2
nkosinathi <nkosinathi.msubo291@gmail.com>
2023-03-28 16:32:53 -0700

    printInt.c

--- printInt.c
+++ printInt.c
@@ -17,7 +17,7 @@ int printInt(va_list arguments, char *buf, unsigned int ibuf)
 	if (int_input < 0)
 	{
 		int_in = int_input * -1;
-		ibuf = handlBuf(buf, '-', ibuf);
+		ibuf = handleBuffer(buf, '-', ibuf);
 		isneg = 1;
 	}
 	else
@@ -36,7 +36,7 @@ int printInt(va_list arguments, char *buf, unsigned int ibuf)
 
 	for (i = 0; div > 0; div /= 10, i++)
 	{
-		ibuf = handlBuf(buf, ((int_in / div) % 10) + '0', ibuf);
+		ibuf = handleBuffer(buf, ((int_in / div) % 10) + '0', ibuf);
 	}
 	return (i + isneg);
 }

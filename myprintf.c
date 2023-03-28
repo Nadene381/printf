fea15c3a4081323baffc38c0c1e4dca8263a68e2
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-28 10:49:58 -0700

    Printf Function

--- myprintf.c
+++ myprintf.c
@@ -8,34 +8,35 @@
 */
 int _printf(const char *format, ...)
 {
-   va_list arg_list;
-   va_start(arg_list, format);
-   while (*format != '\0') {
-       if (*format == '%') {
-           format++;
-           if (*format == 'd') {
-               int val = va_arg(arg_list, int);
-               printf("%d", val);
-           }
-           else if (*format == 's') {
-               char* val = va_arg(arg_list, char*);
-               printf("%s", val);
-           }
-           else if (*format == 'c') {
-               char val = va_arg(arg_list, int);
-               printf("%c", val);
-           }
-           else if (*format == 'f') {
-               double val = va_arg(arg_list, double);
-               printf("%f", val);
-           }
-           format++;
-       }
-       else {
-           printf("%c", *format);
-           format++;
-       }
-   }
-   va_end(arg_list);
-   return (1);
+va_list arg_list;
+va_start(arg_list, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+if (*format == 's')
+{
+char *val = va_arg(arg_list, char*);
+printf("%s", val);
+}
+else if (*format == 'c')
+{
+char val = va_arg(arg_list, int);
+printf("%c", val);
+}
+else if (*format == '%')
+{
+printf("%c", 37);
+}
+format++;
+}
+else
+{
+printf("%c", *format);
+format++;
+}
+}
+va_end(arg_list);
+return (1);
 }

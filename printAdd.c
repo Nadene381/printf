c3c3cc47cb45d28445abe76001c3bbc6fd7c60b9
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-28 16:37:26 -0700

    Printf Function

--- printAdd.c
+++ printAdd.c
@@ -20,7 +20,7 @@ int printAdd(va_list arguments, char *buf, unsigned int ibuf)
 	if (add == NULL)
 	{
 		for (i = 0; nill[i]; i++)
-			ibuf = handl_buf(buf, nill[i], ibuf);
+			ibuf = handleBuffer(buf, nill[i], ibuf);
 		return (5);
 	}
 	int_input = (intptr_t)add;
@@ -34,15 +34,15 @@ int printAdd(va_list arguments, char *buf, unsigned int ibuf)
 	binary = binaryArray(binary, int_input, isnegative, 64);
 	hexadecimal = malloc(sizeof(char) * (16 + 1));
 	hexadecimal = hexArray(binary, hexadecimal, 0, 16);
-	ibuf = handlBuf(buf, '0', ibuf);
-	ibuf = handlBuf(buf, 'x', ibuf);
+	ibuf = handleBuffer(buf, '0', ibuf);
+	ibuf = handleBuffer(buf, 'x', ibuf);
 	for (first_digit = i = count = 0; hexadecimal[i]; i++)
 	{
 		if (hexadecimal[i] != '0' && first_digit == 0)
 			first_digit = 1;
 		if (first_digit)
 		{
-			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
+			ibuf = handleBuffer(buf, hexadecimal[i], ibuf);
 			count++;
 		}
 	}

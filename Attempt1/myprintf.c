c19abdbdbde9ae5c367fd567c5bd85843ca365f5
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-28 18:44:57 -0700

    _Printf Function

---
+++ Attempt1/myprintf.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+/**
+ * _printf - formatted output conversion and print data.
+ * @format: Character string.
+ * Return: 1.
+ */
+int _printf(const char *format, ...)
+{
+unsigned int n = 0, lenght = 0, buf = 0;
+va_list arguments;
+int (*function)(va_list, char *, unsigned int);
+char *buffer;
+va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+if (!format || !buffer || (format[n] == '%' && !format[n + 1]))
+return (-1);
+if (!format[n])
+return (0);
+for (n = 0; format && format[n]; n++)
+{
+if (format[n] == '%')
+{
+if (format[n + 1] == '\0')
+{
+print_buf(buffer, buf), free(buffer), va_end(arguments);
+return (-1);
+}
+else
+{
+function = getFunct(format, n + 1);
+if (function == NULL)
+{
+if (format[n + 1] == ' ' && !format[n + 2])
+return (-1);
+handleBuffer(buffer, format[n], buf), lenght++, n--;
+}
+else
+{
+lenght += function(arguments, buffer, buf);
+n += printFunc(format, n + 1);
+}
+}
+n++;
+}
+else
+handleBuffer(buffer, format[n], buf), lenght++;
+for (buf = lenght; buf > 1024; buf -= 1024)
+;
+}
+print_buf(buffer, buf), free(buffer), va_end(arguments);
+return (lenght);
+}
---
+++ Attempt1/printAdd.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * printAdd - prints the address of an input variable
+ * @arguments: input address.
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer
+ *
+ * Return: number of chars printed.
+ */
+int printAdd(va_list arguments, char *buf, unsigned int ibuf)
+{
+	void *add;
+	long int int_input;
+	int i, count, first_digit, isnegative;
+	char *hexadecimal, *binary;
+	char nill[] = "(nil)";
+
+	add = (va_arg(arguments, void *));
+	if (add == NULL)
+	{
+		for (i = 0; nill[i]; i++)
+			ibuf = handleBuffer(buf, nill[i], ibuf);
+		return (5);
+	}
+	int_input = (intptr_t)add;
+	isnegative = 0;
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+	binary = malloc(sizeof(char) * (64 + 1));
+	binary = binaryArray(binary, int_input, isnegative, 64);
+	hexadecimal = malloc(sizeof(char) * (16 + 1));
+	hexadecimal = hexArray(binary, hexadecimal, 0, 16);
+	ibuf = handleBuffer(buf, '0', ibuf);
+	ibuf = handleBuffer(buf, 'x', ibuf);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	{
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handleBuffer(buf, hexadecimal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(hexadecimal);
+	return (count + 2);
+}
